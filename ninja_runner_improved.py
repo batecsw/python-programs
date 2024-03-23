819206396d6970a6f34d6fd2a6da9d0915079b93
batecsw <batecsw@hotmail.com>
2024-03-23 20:56:53 +0000

    Update ninja_runner_improved.py

--- ninja_runner_improved.py
+++ ninja_runner_improved.py
@@ -1,5 +1,6 @@
 # Write your code here :-)
 from pgzhelper import *
+import random
 
 WIDTH=800
 HEIGHT=600
@@ -14,32 +15,36 @@ velocity_y = 0
 gravity = 1
 
 obstacles = []
-obstacles_timeout = 0
+obstacles_timeout = 50
 
 score = 0
 game_over = False
+lives = 5
 
 def update():
-  global velocity_y, obstacles_timeout, score, game_over
+  global velocity_y, obstacles_timeout, score, game_over, lives
   runner.next_image()
 
   if game_over == False:
-      obstacles_timeout += 1
-      if obstacles_timeout > 50:
+      obstacles_timeout -= 1
+      if obstacles_timeout < 1:
         actor = Actor('cactus')
         actor.x = 850
         actor.y = 430
         obstacles.append(actor)
-        obstacles_timeout = 0
+        obstacles_timeout = random.randint(50, 100)
 
       for actor in obstacles:
-        actor.x -= 8
+        actor.x = actor.x-5-score/10
         if actor.x < -50:
-          obstacles.remove(actor)
-          score += 1
+            obstacles.remove(actor)
+            score += 1
+        if actor.colliderect(runner):
+            lives -= 1
+            obstacles.remove(actor)
 
-      if keyboard.up:
-        velocity_y = -15
+      if keyboard.up and runner.y > 380:
+        velocity_y = -20
 
       runner.y += velocity_y
       velocity_y += gravity
@@ -47,7 +52,7 @@ def update():
         velocity_y = 0
         runner.y = 400
 
-  if runner.collidelist(obstacles) != -1:
+  if lives < 1:
     game_over = True
 
 def draw():
@@ -61,6 +66,5 @@ def draw():
     for actor in obstacles:
       actor.draw()
     screen.draw.text('Score: ' + str(score), (15,10), color=(0,0,0), fontsize=30)
-
-pgzrun.go() # Must be last line
+    screen.draw.text('Lives: ' + str(lives), (115,10), color=(0,0,0), fontsize=30)
 
